9311e5dc6cd5c4f95a2a0cce184ca39cd8b6e6ad
Dorde Kesic <dordekesic.keso@gmail.com>
2025-11-30 21:42:56 +0100

    adds prettier tailwind plugin for sorting class names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div className="py-2 md:py-5">
             <div className="text-center">
-                <h1 className="text-4xl md:text-5xl mb2">Discover Movies</h1>
+                <h1 className="mb2 text-4xl md:text-5xl">Discover Movies</h1>
                 <p>Search and explore popular films</p>
             </div>
         </div>
